40eb938d8d90de5cf31bbc02cd9056d05b7f374a
Millergy <emil@mindata.se>
2025-04-11 15:18:20 +0200

    Now it works as before while I can work on it

--- Log_class.py
+++ Log_class.py
@@ -26,37 +26,11 @@ from functions import try_int_float_convert
 class Log:
 
     def __init__(self, filepath, general_log_filename, show_progress = True):
-        # Init dicts for data import
-        self.data = {}      # Log data
-        self.other = {}     # Data not useful at this time
-        self.time_data = {} # Time stats
         
-        # Go through all files in folder
-        if show_progress:
-            for log_filename in tqdm(os.listdir(filepath), desc="Importing data"):
-                self.import_file(filepath, log_filename)
-        else:
-            for log_filename in os.listdir(filepath):
-                self.import_file(filepath, log_filename)
-
-        # For testing
-        # self.import_file(filepath, general_log_filename)
-        # self.import_file(filepath, "camera_0_output-fast.log", 150)
-
-        
-        # Put general data in seperate dict
-        self.general_data = self.data[general_log_filename]
-        del self.data[general_log_filename]
-        self.format_general()
-
-        if show_progress:
-            for i in tqdm(self.data, desc="Formatting data"):
-                self.format_data(i)
-        else:
-            for i in self.data:
-                self.format_data(i)
-
-    def __init__new(self, filepath, general_log_filename, show_progress = True):
+        # If in optimized mode, run prod code
+        if not __debug__:
+            self.init_old(filepath, general_log_filename, show_progress)
+            return
 
         # Go through all files in folder
         if show_progress:
@@ -78,10 +52,36 @@ class Log:
         # Format data
         if show_progress:
             for i in tqdm(self.data.keys(), desc="Formatting data"):
-                self.format_data(i)
+                self.data[i] = self.format_data(data[i])
         else:
             for i in self.data.keys():
-                self.format_data(i)
+                self.data[i] = self.format_data(data[i])
+
+    def init_old(self, filepath, general_log_filename, show_progress = True):
+        # Init dicts for data import
+        self.data = {}      # Log data
+        self.other = {}     # Data not useful at this time
+        self.time_data = {} # Time stats
+        
+        # Go through all files in folder
+        if show_progress:
+            for log_filename in tqdm(os.listdir(filepath), desc="Importing data"):
+                self.import_file_old(filepath, log_filename)
+        else:
+            for log_filename in os.listdir(filepath):
+                self.import_file_old(filepath, log_filename)
+        
+        # Put general data in seperate dict
+        self.general_data = self.data[general_log_filename]
+        del self.data[general_log_filename]
+        self.format_general()
+
+        if show_progress:
+            for i in tqdm(self.data, desc="Formatting data"):
+                self.format_data_old(i)
+        else:
+            for i in self.data:
+                self.format_data_old(i)
 
 #%% return data
     # name of folder with archived logs, it is the timestamp of the log
@@ -105,7 +105,29 @@ class Log:
 
 #%% used for __init__
     # Convert text in files to dict
-    def import_file(self, folder_filepath, filename, limit = None):
+    def import_file(self, folder_filepath, filename):
+        # If log data does not follow expected fomrat it is put in "other"
+        # Used for debugging
+        self.other[filename] = []
+        data = {}
+
+        filepath = os.path.join(folder_filepath, filename)
+        with open(filepath, "r", encoding="utf-8") as file:
+            for i,line in enumerate(file):
+
+                # ':' represents data, otherwise it is just info and is put in 'other'
+                if ':' in line:
+                    # Group data by key, the key is the string before the first ':'
+                    # For every key there is a list with the values
+                    key, value = line.strip().split(":", 1)
+                    data.setdefault(filename, {}).setdefault(key, []).append(value.strip())
+
+                elif not line in self.other[filename]:
+                    self.other[filename].append(line)
+        
+        return data
+    
+    def import_file_old(self, folder_filepath, filename, limit = None):
         # If log data does not follow expected fomrat it is put in "other"
         # Used for debugging
         self.other[filename] = []
@@ -166,7 +188,7 @@ class Log:
         return time_list
 
     # formats data into dicts for plotting
-    def format_data(self, key):
+    def format_data_old(self, key):
         data_dict = self.data[key].copy()
         value_dict = {}
 
@@ -227,5 +249,14 @@ class Log:
             self.time_data[key] = value_dict
 
     # formats data into dicts for plotting
-    def format_data_new(self, key):
+    def format_data(self, data): # data is values from one file as a dict
+        formatted_data = {}
 
+        for i,key in enumerate(data):
+            sub_data = data[i]  # List of all elements
+
+            split_array = list(map(lambda item: item.split(","), sub_data))
+            print(split_array)
+
+
+        return formatted_data
---
+++ test.py
@@ -0,0 +1,5 @@
+array = ["hej,då", "ren,bert"]
+
+array = ["hej,då", "ren,bert"]
+split_array = list(map(lambda item: item.split(","), array))
+print(split_array)
